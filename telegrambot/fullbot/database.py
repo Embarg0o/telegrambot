6dc7f8fae04ceb55b63996f39b1225f101b739f4
Andrey Moblyan <Movlyanandrew1993@mail.ru>
2024-02-11 18:30:44 +0300

    Implementation of statistics

--- telegrambot/fullbot/database.py
+++ telegrambot/fullbot/database.py
@@ -10,11 +10,24 @@ async def create_tables():
         await db.execute('''CREATE TABLE IF NOT EXISTS quiz_results (user_id INTEGER PRIMARY KEY, score INTEGER)''')
         await db.commit()
 
-async def update_quiz_results(user_id, score):
+async def save_user_score(user_id, score):
     async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute('''
-            INSERT OR REPLACE INTO quiz_results (user_id, score) VALUES (?, ?)
-        ''', (user_id, score))
+        await db.execute('''INSERT OR REPLACE INTO quiz_results (user_id, score) VALUES (?, ?)''', (user_id, score))
+        await db.commit()
+
+async def get_user_last_score(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute('SELECT score FROM quiz_results WHERE user_id = (?)', (user_id, )) as cursor:
+            result = await cursor.fetchone()
+            if result is not None:
+                return result[0]
+            else:
+                return 0
+
+
+async def update_quiz_score(user_id, score):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute('INSERT OR REPLACE INTO quiz_results (user_id, score) VALUES (?, ?)', (user_id, score))
         await db.commit()
 
 async def get_quiz_index(user_id):
@@ -28,8 +41,6 @@ async def get_quiz_index(user_id):
                 return results[0]
             else:
                 return 0
-
-
 
 async def update_quiz_index(user_id, index):
     # Создаем соединение с базой данных (если она не существует, она будет создана)
@@ -63,4 +74,4 @@ async def new_quiz(message):
 
     # запрашиваем новый вопрос для квиза
     await get_question(message, user_id)
-
+    
--- telegrambot/fullbot/telebot.py
+++ telegrambot/fullbot/telebot.py
@@ -1,12 +1,11 @@
 import asyncio
 import logging
-import aiosqlite
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from database import create_tables, new_quiz
 from aiogram import F
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from database import update_quiz_index, get_quiz_index, get_question, new_quiz, update_quiz_results
+from database import update_quiz_index, get_quiz_index, get_question, new_quiz, get_user_last_score, save_user_score
 from quiz_data import quiz_data
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -20,7 +19,6 @@ dp = Dispatcher()
 
 DB_NAME = 'quiz_bot.db'
 
-
 @dp.callback_query(F.data == "right_answer")
 async def right_answer(callback: types.CallbackQuery):
     await callback.bot.edit_message_reply_markup(
@@ -28,43 +26,22 @@ async def right_answer(callback: types.CallbackQuery):
         message_id=callback.message.message_id,
         reply_markup=None
     )
-
     current_question_index = await get_quiz_index(callback.from_user.id)
-
+    
     correct_option = quiz_data[current_question_index]['correct_option']
     await callback.message.answer(f"Выбранный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
 
     await callback.message.answer("Верно!")
-
-    # await update_quiz_results(callback.from_user.id, score)
 
     current_question_index += 1
     await update_quiz_index(callback.from_user.id, current_question_index)
 
     if current_question_index < len(quiz_data):
         await get_question(callback.message, callback.from_user.id)
+        score = await get_user_last_score(callback.from_user.id) 
+        await save_user_score(callback.from_user.id, score)    
     else:
-        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
-    
-    # Получение текущего пользователя
-    user_id = callback.from_user.id
-
-    # Увеличение счета игрока на 1
-    async with aiosqlite.connect(DB_NAME) as db:
-        async with db.execute('SELECT score FROM quiz_results WHERE user_id = ?', (user_id,)) as cursor:
-            result = await cursor.fetchone()
-
-            if result is not None:
-                # Если результат уже существует, увеличиваем счет
-                score = result[0] + 1
-                await db.execute('UPDATE quiz_results SET score = ? WHERE user_id = ?', (score, user_id))
-            else:
-                # Если результат не существует, создаем новую запись
-                score = 1
-                await db.execute('INSERT INTO quiz_results (user_id, score) VALUES (?, ?)', (user_id, score))
-
-        await db.commit()
-
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")              
 
 @dp.callback_query(F.data == "wrong_answer")
 async def wrong_answer(callback: types.CallbackQuery):
@@ -88,16 +65,15 @@ async def wrong_answer(callback: types.CallbackQuery):
         await get_question(callback.message, callback.from_user.id)
     else:
         await callback.message.answer("Это был последний вопрос. Квиз завершен!")
-   
-
-
+        
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     # Создаем сборщика клавиатур типа Reply
     builder = ReplyKeyboardBuilder()
-    # Добавляем в сборщик одну кнопку
+    # Добавляем в сборщик две кнопки
     builder.add(types.KeyboardButton(text="Начать игру"))
+    builder.add(types.KeyboardButton(text="Статистика"))
     # Прикрепляем кнопки к сообщению
     await message.answer("Добро пожаловать в квиз!", reply_markup=builder.as_markup(resize_keyboard=True))
 
@@ -109,23 +85,12 @@ async def cmd_quiz(message: types.Message):
     await message.answer(f"Давайте начнем квиз!")
     await new_quiz(message)
 
+@dp.message(F.text=="Статистика")
 @dp.message(Command("stats"))
 async def cmd_stats(message: types.Message):
-    async with aiosqlite.connect(DB_NAME) as db:
-        async with db.execute('''
-            SELECT user_id, score FROM quiz_results ORDER BY score DESC
-        ''') as cursor:
-            results = await cursor.fetchall()
-
-    # Выведи статистику игроков
-    if len(results) == 0:
-        await message.answer("Пока нет результатов прохождения квиза.")
-    else:
-        statistics = []
-        for row in results:
-            user = await bot.get_chat(row[0])
-            statistics.append(f"@{user.username}: {row[1]}")
-        await message.answer("Статистика игроков:\n\n" + "\n".join(statistics))
+    user_id = message.from_user.id
+    score = await get_user_last_score(user_id)
+    await message.answer(f"Ваш результат: {score}")
 
 async def main():
 
